a383cc13f6926cadc0e5a4a9c58308abec84e440
Gardeniaa101 <onejwon33@naver.com>
2025-02-13 13:22:39 +0900

    Setting
    - Setting the project
    - foldering

---
+++ BBIP-Clone/BBIP-Clone/BBIP_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBIP_CloneApp.swift
+//  BBIP-Clone
+//
+//  Created by HanJW on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BBIP_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBIP-Clone/BBIP-Clone/Resources/Literals/ColorLiterals.swift
@@ -0,0 +1,7 @@
+//
+//  ColorLiterals.swift
+//  BBIP-Clone
+//
+//  Created by HanJW on 2/12/25.
+//
+
