0d0216b7b64b60ed325011c27fba81f4d4a05ec7
Gardeniaa101 <onejwon33@naver.com>
2025-03-08 23:24:39 +0900

    [feat] TitleView, TitleButtonView 하단 Spacer 추가

--- BBIP-Clone/BBIP-Clone/Presentation/Core/TitleView.swift
+++ BBIP-Clone/BBIP-Clone/Presentation/Core/TitleView.swift
@@ -11,15 +11,20 @@ struct TitleView: View {
     let title: String
     
     var body: some View {
-        HStack {
-            Spacer()
-                .frame(width: 28)
-
-            Text(title)
-                .font(.wantedSansBold(ofSize: 16))
-                .frame(height: 19, alignment: .leading)
+        VStack {
+            HStack {
+                Spacer()
+                    .frame(width: 28)
+                
+                Text(title)
+                    .font(.wantedSansBold(ofSize: 16))
+                    .frame(height: 19, alignment: .leading)
+                
+                Spacer()
+            }
             
             Spacer()
+                .frame(height: 12)
         }
     }
 }
