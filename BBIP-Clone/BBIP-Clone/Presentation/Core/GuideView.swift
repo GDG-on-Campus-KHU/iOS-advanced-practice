a6e026152f82cb7ae58ee6ef3463d3d91c6597ed
binschocolate <hisbulletp@gmail.com>
2025-03-09 19:01:07 +0900

    guideview 수정완

--- BBIP-Clone/BBIP-Clone/Presentation/Core/GuideView.swift
+++ BBIP-Clone/BBIP-Clone/Presentation/Core/GuideView.swift
@@ -14,21 +14,27 @@ struct GuideView: View {
             RoundedRectangle(cornerRadius: 12)
                 .frame(width: 359, height: 79)
                 .foregroundStyle(Color.mainWhite)
-            HStack{
+            HStack(spacing: 80){
                 VStack(alignment: .leading, spacing: 1){
                     Text("삡이 처음이라면?")
                         .foregroundStyle(Color.primary3)
                         .font(.wantedSansMedium(ofSize: 14))
-                    
+                    HStack{
                         Text("BBIP 이용 가이드 살펴보기")
                             .foregroundStyle(Color.mainBlack)
                             .font(.wantedSansBold(ofSize: 16))
-                        
-                    
+                        Image("Group 632595")
+                    }
                 }
-                .padding()
+                .padding(10)
                 
                 
+            
+            
+                Image("logo") // 앱 아이콘 추가
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 60)
                     
             }
             
